63e5e4d698087f5c6bff074e7342baf1be19e591
부엉이 <o.o.wl.soft@gmail.com>
2019-12-08 02:20:45 +0900

    feat: Add BeautifulString

--- Source/BeautifulString.swift
+++ Source/BeautifulString.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+// MARK: - Attribute
+
+public typealias Attribute = (key: NSAttributedString.Key, value: Any)
+
+// MARK: - BeautifulString
+
+public typealias BeautifulString = NSMutableAttributedString
+
+// MARK: - BeautifulString
+
+extension BeautifulString {
+    
+    var range: NSRange {
+        return NSRange(location: 0, length: self.length)
+    }
+}
+
+// MARK: - BeautifulString
+
+extension BeautifulString {
+    
+    public func addAttribute(_ attribute: Attribute, range: NSRange) {
+        addAttribute(attribute.key, value: attribute.value, range: range)
+    }
+}
