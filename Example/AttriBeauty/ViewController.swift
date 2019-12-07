e3becf50ec1956c176657a1ee871572d62b6c01c
부엉이 <o.o.wl.soft@gmail.com>
2019-12-08 02:22:22 +0900

    feat: Add code to  apply demo

--- Example/AttriBeauty/ViewController.swift
+++ Example/AttriBeauty/ViewController.swift
@@ -7,18 +7,29 @@
 //
 
 import UIKit
+import AttriBeauty
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var demoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        demoLabel.attributedText = demoLabel.text?.beautfiy()
+            .attriBeauty
+            .fgColor(.blue)
+            .bgColor(.purple).regex("y")
+            .fgColor(.red).range(NSRange(0...4))
+            .font(.boldSystemFont(ofSize: 30))
+            .align(.center)
+            .apply()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
 
